30ebfd0243c9f8abf4f4eed6350e76276c7dde37
josedrios <joserios0531@gmail.com>
2025-08-01 15:08:36 -0700

    Cleaned up file structure, removing useless files, added sass/styles structure and files, did basic layout styling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <header>
+        {/* header */}
+        {/* nav */}
+        my gamer cred
+      </header>
+      <div>{/* page related */}</div>
+    </>
+  );
+}
+
+export default App;
