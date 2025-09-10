f214924f6be2818691eecfff76f438c2bdc7575f
josedrios <joserios0531@gmail.com>
2025-09-10 09:38:37 -0700

    Added nav hover animations, added transition variables to variables.scss

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,27 +1,30 @@
 import { NavLink } from 'react-router-dom';
+import { IoCubeOutline } from 'react-icons/io5';
+import { IoPersonOutline } from 'react-icons/io5';
+import { IoSearchOutline } from 'react-icons/io5';
 
 export default function Nav() {
   return (
     <nav>
-      <div id='nav-container'>
-        <NavLink id="nav-home" to="/">
-          <img className="pixel-art" src="/images/gc.png" alt="" />
-          <h1>MYGAMERCRED</h1>
+      <NavLink id="nav-home" to="/">
+        <IoCubeOutline />
+      </NavLink>
+      <div id="nav-body">
+        <NavLink className="nav-link" to="/lists">
+          <IoSearchOutline />
         </NavLink>
-        <div id="nav-body">
-          <NavLink className="nav-link" to="/games">
-            GAMES
-          </NavLink>
-          <NavLink className="nav-link" to="/lists">
-            LISTS
-          </NavLink>
-          <NavLink className="nav-link" to="/players">
-            PLAYERS
-          </NavLink>
-          <NavLink className="nav-link" to="/lists">
-            SIGN UP
-          </NavLink>
-        </div>
+        <NavLink className="nav-link" to="/games">
+          GAMES
+        </NavLink>
+        <NavLink className="nav-link" to="/lists">
+          LISTS
+        </NavLink>
+        <NavLink className="nav-link" to="/players">
+          PLAYERS
+        </NavLink>
+        <NavLink className="nav-link" to="/players">
+          <IoPersonOutline />
+        </NavLink>
       </div>
     </nav>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,28 @@
-import WaveText from '../components/UI/WaveText';
+import { IoCubeOutline } from "react-icons/io5";
+
 export default function Home() {
   return (
-    <>
-      <main>
-        main
-      </main>
-      <aside>
-        aside
-      </aside>
-    </>
+    <main>
+      {/* <h1>GAME REVIEW SHARE</h1> */}
+      {/* <h1>Welcome to MyGamerCred.</h1>
+      <p>
+        Are you tired of reading the most sellout reviews? Do you want to give
+        the gaming community a piece of your mind?
+      </p>
+      <p>
+        Look no further. This is where you go to find and give honest video game
+        reviews from real gamers. Also add games to your account to show off
+        what type of individual you really are.
+      </p>
+      <h2>#INCLUDE TYPING AND DELETING ANIMATION HERE#</h2>
+      {/* This game is {options} */}
+      {/* EXAMPLES FOR OPTIONS: TRASH!!, confusing..., f**kin weird,  */}
+      {/* <h2 className="home-right">What is Gamer Cred?</h2>
+      <p className="home-right">Gamer cred is the most valuable currency known to man:</p>
+      <div className="home-right" id="gc-showcase">
+        <IoCubeOutline />
+      </div> */} 
+
+    </main>
   );
 }
