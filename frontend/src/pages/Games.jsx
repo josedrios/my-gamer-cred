db1ae06d54e5a8054dc0f81f979ff65487c43c50
josedrios <joserios0531@gmail.com>
2025-09-22 13:22:43 -0700

    Created basic layout for game sections, individual game cards not styled yet

--- frontend/src/pages/Games.jsx
+++ frontend/src/pages/Games.jsx
@@ -6,6 +6,23 @@ export default function Games() {
   return (
     <main>
       <SearchForm filters={filters} placeholder={"Search for games..."}/>
+      <GameSection />
     </main>
   );
+}
+
+function GameSection() {
+  return (
+    <section className="game-section">
+      <h3>Trending in September</h3>
+      <div className="game-section-children">
+        <div className="test" />
+        <div className="test" />
+        <div className="test" />
+        <div className="test" />
+        <div className="test" />
+        <div className="test" />
+      </div>
+    </section>
+  )
 }
