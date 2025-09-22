6af31763c1111d6c7873e8e4bf11b77571cbc653
josedrios <joserios0531@gmail.com>
2025-09-22 15:33:38 -0700

    Game cards have been implemented, along with their hover effects (hiding and showing select components), hover button section has not been worked on yet

--- frontend/src/pages/Games.jsx
+++ frontend/src/pages/Games.jsx
@@ -1,28 +1,59 @@
-import SearchForm from "../components/Search/SearchForm";
+import { IoCubeOutline } from 'react-icons/io5';
+import { IoCheckmark } from 'react-icons/io5';
+import { IoEllipsisVertical } from 'react-icons/io5';
+import SearchForm from '../components/Search/SearchForm';
+import batman from '../assets/images/game_cover.jpg';
+import elden from '../assets/images/game_cover2.jpg';
+import titan from '../assets/images/game_cover3.jpg';
+import lbp from '../assets/images/game_cover4.jpg';
+import mgs from '../assets/images/game_cover5.jpg';
+import re from '../assets/images/game_cover6.jpg';
 
 export default function Games() {
-  const filters = ['rating', 'time', 'popular', 'genre']
+  const filters = ['rating', 'time', 'popular', 'genre'];
 
   return (
     <main>
-      <SearchForm filters={filters} placeholder={"Search for games..."}/>
-      <GameSection />
+      <SearchForm filters={filters} placeholder={'Search for games...'} />
+      <GameSection title={'Trending in September'} />
+      <GameSection title={'Recently Reviewed'} />
     </main>
   );
 }
 
-function GameSection() {
+function GameSection({ title }) {
   return (
     <section className="game-section">
-      <h3>Trending in September</h3>
+      <h3>{title}</h3>
       <div className="game-section-children">
-        <div className="test" />
-        <div className="test" />
-        <div className="test" />
-        <div className="test" />
-        <div className="test" />
-        <div className="test" />
+        <GameCard source={elden} title="Elden Ring" />
+        <GameCard source={batman} title="Batman Arkham Asylum" />
+        <GameCard source={titan} title="Titan Fall 2" />
+        <GameCard source={lbp} title="LittleBigPlanettttttt" />
+        <GameCard source={mgs} title="Metal Gear Solid 3: Snake Eater" />
+        <GameCard source={re} title="Resident Evil 2 Remake" />
       </div>
     </section>
-  )
+  );
+}
+
+function GameCard({ source, title = 'Temporary' }) {
+  return (
+    <div className="game-card">
+      <div className="game-card-body">
+        <img src={source} alt="" />
+        <p className="game-card-title">{title}</p>
+        <p className="game-card-gc">
+          <IoCubeOutline />
+          100
+        </p>
+        <div className='game-card-options'>
+          <IoCheckmark />
+          <IoCubeOutline />
+          <IoEllipsisVertical />
+        </div>
+      </div>
+      <p className="player-count">627k Players</p>
+    </div>
+  );
 }
