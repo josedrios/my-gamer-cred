a2d4aacce558b433129737c9a98ac47c80c43a8f
Jose Rios <joserios0531@gmail.com>
2025-09-25 10:41:59 -0700

    Styled game posters to overlap alongside eachother for list cards

--- frontend/src/pages/Lists.jsx
+++ frontend/src/pages/Lists.jsx
@@ -1,3 +1,10 @@
+import batman from '../assets/images/game_cover.jpg';
+import elden from '../assets/images/game_cover2.jpg';
+import titan from '../assets/images/game_cover3.jpg';
+import lbp from '../assets/images/game_cover4.jpg';
+import mgs from '../assets/images/game_cover5.jpg';
+import re from '../assets/images/game_cover6.jpg';
+
 export default function Lists() {
   return (
     <main>
@@ -15,19 +22,36 @@ function ListSection({ title }) {
         <ListCard />
         <ListCard />
         <ListCard />
-        <ListCard />
-        <ListCard />
-        <ListCard />
       </div>
     </div>
   );
 }
 
 function ListCard() {
+  const posters = [titan, lbp, mgs, re];
+
   return (
     <div className="list-card">
-      <p className="list-game-count">6 games</p>
-      <div className="list-poster-cards"></div>
+      <div className="list-posters">
+        {posters.map((poster, i) => (
+          <img
+            src={poster}
+            alt=""
+            style={{
+              left: `${i * 60}px`,
+              zIndex: 1000 - i,
+            }}
+          />
+        ))}
+        <p
+          className="list-game-count"
+          style={{
+            zIndex: 1001,
+          }}
+        >
+          6 games
+        </p>
+      </div>
       <p className="list-card-title">My Game List</p>
       <div className="list-user-container">
         <div className="list-user-pfp" />
