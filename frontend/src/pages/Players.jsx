fb7da68bdd30b69d42d148b7c00f7728ffceb524
josedrios <joserios0531@gmail.com>
2025-09-30 10:11:00 -0700

    Styled player card to have hover effect and cleaned up player gc and list count on the player cards

--- frontend/src/pages/Players.jsx
+++ frontend/src/pages/Players.jsx
@@ -1,28 +1,38 @@
 import { SectionRow } from '../components/UI/Sections.jsx';
 import { RowCard } from '../components/UI/Cards.jsx';
+import { IoCubeOutline } from 'react-icons/io5';
 
 export default function Players() {
   return (
     <main>
-      <SectionRow title={'Popular Players'}>
+      <PlayersSection title='On the Rise'/>
+      <PlayersSection title='Popular Players'/>
+    </main>
+  );
+}
+
+function PlayersSection({title= 'NO TITLE'}) {
+  return (
+      <SectionRow title={title}>
         <PlayerCard username="LoreConsumer" gc={2987} games={427} />
         <PlayerCard username="LoreConsumer" gc={2987} games={427} />
         <PlayerCard username="LoreConsumer" gc={2987} games={427} />
         <PlayerCard username="LoreConsumer" gc={2987} games={427} />
         <PlayerCard username="LoreConsumer" gc={2987} games={427} />
       </SectionRow>
-    </main>
-  );
+  )
 }
 
 function PlayerCard({ username = 'UKNOWN', gc = 999, games = 999 }) {
   return (
     <RowCard type={'player'}>
       <div className="row-card__pfp" />
-      <p>{username}</p>
+      <p className='row-card__username'>{username}</p>
       <div className="row-card__user-misc">
-        <p className="row-card__gc">{gc}</p>
-        <p>&nbsp;/&nbsp;</p>
+        <p className="row-card__gc">
+          <IoCubeOutline /> {gc}
+        </p>
+        <p>&nbsp;|&nbsp;</p>
         <p className="row-card__game-count">{games} games</p>
       </div>
     </RowCard>
