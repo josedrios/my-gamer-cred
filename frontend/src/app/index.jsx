5d0a53129017fc8e39775d6e604c2d3361eab404
josedrios <joserios0531@gmail.com>
2025-10-07 12:59:48 -0700

    Cleaned up nav by using tailwind, cutting down on the css there, still getting a feel for it, feels nice tho

--- frontend/src/app/index.jsx
+++ frontend/src/app/index.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppRouter from '@/app/router';
 import '@/styles/main.scss'
-import '@/index.css'
+import '@/tailwind.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -9,42 +9,82 @@ import { useLocation } from 'react-router-dom';
 export default function Nav() {
   const location = useLocation();
   const path = location.pathname;
+  const iconClass = 'group-hover:stroke-primary text-lg';
+  const navLinks = [
+    {
+      to: '/search',
+      icon: <IoSearchOutline className={iconClass} />,
+    },
+    { to: '/games', label: 'games' },
+    { to: '/lists', label: 'lists' },
+    { to: '/players', label: 'players' },
+    {
+      to: '/account',
+      icon: <IoPersonOutline className={iconClass} />,
+    },
+  ];
 
   return (
-    <motion.nav className='nav' variants={container} initial="hidden" animate="show">
-      <motion.div className='nav__logo' variants={fadeIn}>
-        <NavLink id="nav-home" to="/">
-          <IoCubeOutline />
+    <motion.nav
+      className="nav flex py-3 m-auto mt-3 max-w-screen-lg max-xs:flex-col"
+      variants={container}
+      initial="hidden"
+      animate="show"
+    >
+      <motion.div
+        className="nav__logo flex items-center max-xs:m-auto max-xs:mb-4"
+        variants={fadeIn}
+      >
+        <NavLink to="/">
+          <IoCubeOutline className="text-3xl stroke-primary" />
         </NavLink>
       </motion.div>
-      <motion.div variants={fadeIn} className="nav__body">
-        <NavLink className="nav__link nav__link--icon" to="/search">
-          <IoSearchOutline />
-        </NavLink>
-        <NavLink className="nav__link nav__link--non-icon" to="/games">
-          <span className={path !== '/games' ? `hide` : ``}>#</span>
-          GAMES
-        </NavLink>
-        <NavLink className="nav__link nav__link--non-icon" to="/lists">
-          <span className={path !== '/lists' ? `hide` : ``}>#</span>
-          LISTS
-        </NavLink>
-        <NavLink className="nav__link nav__link--non-icon" to="/players">
-          <span className={path !== '/players' ? `hide` : ``}>#</span>
-          PLAYERS
-        </NavLink>
-        <NavLink className="nav__link nav__link--icon" to="/account">
-          <IoPersonOutline />
-        </NavLink>
-        <div className="nav__responsive-container">
-          <NavLink className="nav__link" to="/home">
-            <IoSearchOutline />
+
+      <motion.div
+        variants={fadeIn}
+        className="nav__body flex justify-end items-center gap-5 flex-1 max-xs:justify-between max-[360px]:flex-wrap max-[360px]:gap-2"
+      >
+        {navLinks.map(({ to, icon, label, key }) => (
+          <NavLink
+            to={to}
+            key={key}
+            className={`group text-xs tracking-wider w-min min-w-[25px] whitespace-nowrap max-[360px]:m-auto ${
+              icon
+                ? 'nav__link--icon max-[360px]:hidden'
+                : 'nav__link--non-icon'
+            }`}
+          >
+            {icon ? (
+              icon
+            ) : (
+              <>
+                <span
+                  className={`${
+                    path === to ? 'text-primary' : 'text-transparent'
+                  } group-hover:text-primary`}
+                >
+                  #
+                </span>
+                {label.toUpperCase()}
+              </>
+            )}
           </NavLink>
-          <NavLink className="nav__link" to="/account">
-            <IoPersonOutline />
-          </NavLink>
-        </div>
+        ))}
       </motion.div>
+      <ResponsiveNav iconClass={iconClass} />
     </motion.nav>
+  );
+}
+
+function ResponsiveNav({ iconClass }) {
+  return (
+    <div className="hidden w-full flex mt-3 justify-center max-[360px]:flex max-[360px]:gap-4 ">
+      <NavLink to="/home" className="group">
+        <IoSearchOutline className={iconClass} />
+      </NavLink>
+      <NavLink to="/account" className="group">
+        <IoPersonOutline className={iconClass} />
+      </NavLink>
+    </div>
   );
 }
