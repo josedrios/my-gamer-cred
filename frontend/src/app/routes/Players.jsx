6ded447d2335ff96e1b89f31cab0a78d450bdecd
josedrios <joserios0531@gmail.com>
2025-10-29 14:48:35 -0700

    Created player cards, started on universal player variant cards

--- frontend/src/app/routes/Players.jsx
+++ frontend/src/app/routes/Players.jsx
@@ -1,45 +1,29 @@
 import { SectionDivider } from '@/components/layouts/section.jsx';
 import { SectionRow } from '@/components/layouts/section.jsx';
-import {
-  PlayerCardRow,
-  PlayerCardCol,
-} from '@/features/players/components/player-card';
+import { UniversalCard } from '@/components/ui/cards';
+import PlayerCard from '@/features/players/components/player-card';
 
 export default function Players() {
   return (
     <main>
       <SectionRow title="Reviewers of the Month">
-        <PlayerCardRow username="I AM USER" gc={2987} games={427} />
-        <PlayerCardRow username="gamer123" gc={2987} games={427} />
-        <PlayerCardRow username="dragonslayer" gc={2987} games={427} />
-        <PlayerCardRow username="Xx_yolo_xX" gc={2987} games={427} />
-        <PlayerCardRow username="onepiecelover" gc={2987} games={427} />
+        <PlayerCard cov={1} />
+        <PlayerCard cov={2} />
+        <PlayerCard cov={1} />
+        <PlayerCard cov={2} />
       </SectionRow>
       <SectionRow title="Most Followed in October">
-        <PlayerCardRow username="pizzaman415" gc={2987} games={427} />
-        <PlayerCardRow username="johndoe" gc={2987} games={427} />
-        <PlayerCardRow username="minecraft" gc={2987} games={427} />
-        <PlayerCardRow username="i_use_arch_btw" gc={2987} games={427} />
-        <PlayerCardRow username="asd.asd.asd" gc={2987} games={427} />
+        <PlayerCard cov={1} />
+        <PlayerCard cov={2} />
+        <PlayerCard cov={1} />
+        <PlayerCard cov={2} />
       </SectionRow>
 
       <SectionDivider>
         <SectionRow title="Discover Players">
-          <PlayerCardCol />
-          <PlayerCardCol />
-          <PlayerCardCol />
-          <PlayerCardCol />
-          <PlayerCardCol />
-          <PlayerCardCol />
+          <UniversalCard type='player'/>
         </SectionRow>
-        {/* This will be a paragraph about adding friends. Include a link to the
-          search page with the 'player' filter already preset */}
-        <SectionRow title="Find Friends" id="section-find-friends">
-          <p>
-            Compare gamer cred with people you know. See their gaming
-            activity, reviews and more!
-          </p>
-        </SectionRow>
+        <SectionRow title="Featured"></SectionRow>
       </SectionDivider>
     </main>
   );
--- frontend/src/features/players/components/player-card.jsx
+++ frontend/src/features/players/components/player-card.jsx
@@ -1,42 +1,35 @@
-import { randColor } from '@/temp-util';
-import { IoCubeOutline, IoPeopleOutline } from 'react-icons/io5';
+import profileCover from '@/assets/images/profile_cover.jpg';
+import profileCover2 from '@/assets/images/profile_cover2.jpg';
+import { rand, randColor } from '@/temp-util';
+import { IoCubeOutline, IoAdd } from 'react-icons/io5';
 
-function PlayerCardRow({ username = 'UKNOWN', gc = 999, games = 999 }) {
+export default function PlayerCard({ cov }) {
   return (
-    <div className="row-card row-card--player">
-      <div className="row-card__pfp" style={{ backgroundColor: randColor() }} />
-      <p className="row-card__username">{username}</p>
-      <div className="row-card__user-misc">
-        <p className="row-card__gc">
-          <IoCubeOutline /> {gc}
-        </p>
-        <p>&nbsp;|&nbsp;</p>
-        <p className="row-card__game-count">
-          <IoPeopleOutline /> {games}
-        </p>
-        <p className="row-card__add-player">+</p>
+    <div className="card player-card">
+      <div
+        className="player-card__cover"
+        style={{
+          backgroundImage: `url(${cov === 1 ? profileCover : profileCover2})`,
+        }}
+      >
+        <div
+          className="player-card__pfp"
+          style={{ backgroundColor: randColor() }}
+        />
+        <div className="player-card__add">
+          <IoAdd />
+        </div>
+      </div>
+      <div className="player-card__body">
+        <p className="player-card__username">SOME DUDE</p>
+        <div className="player-card__info">
+          <p className='player-card__reviews'>{rand(23,90)} reviews</p> 
+          <p className="player-card__gc">
+            <IoCubeOutline />
+            {rand(300, 999)}
+          </p>
+        </div>
       </div>
     </div>
   );
 }
-
-function PlayerCardCol() {
-  return (
-    <div className="col-card col-card--player">
-      <div className="col-card__pfp" style={{ backgroundColor: randColor() }} />
-      <p className="col-card__username">BASHWIZzzzzzzzzz</p>
-      <p className="col-card__stat">
-        <IoCubeOutline />
-        87.2k
-      </p>
-      <p>&nbsp;</p>
-      <p className="col-card__stat">
-        <IoPeopleOutline />
-        12.1k
-      </p>
-      <p className="col-card__add-player">+</p>
-    </div>
-  );
-}
-
-export { PlayerCardRow, PlayerCardCol };
